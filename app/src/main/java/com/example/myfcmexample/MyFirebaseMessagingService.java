73f430a98125970251152a8e07c0d926aa344208
Raghav Sharma <raghavsharma5102@gmail.com>
2020-03-16 13:00:15 +0530

    Notification

---
+++ app/src/main/java/com/example/myfcmexample/MyFirebaseMessagingService.java
@@ -0,0 +1,9 @@
+package com.example.myfcmexample;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+public class MyFirebaseMessagingService extends  FirebaseMessagingService {
+
+}
